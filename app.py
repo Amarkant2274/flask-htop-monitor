fb411902fcc4963d42a34004807141801d77726c
Amarkant2274 <96686572+Amarkant2274@users.noreply.github.com>
2025-02-22 08:08:20 +0000

    Add Flask application with htop endpoint

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import subprocess
+import datetime
+import getpass
+import pytz
+import os
+
+app = Flask(__name__)
+
+@app.route('/htop')
+def htop():
+    # Get username
+    username = getpass.getuser()
+    
+    # Get IST time
+    ist = pytz.timezone('Asia/Kolkata')
+    server_time = datetime.datetime.now(ist).strftime('%Y-%m-%d %H:%M:%S %Z')
+    
+    # Get top output
+    top_output = subprocess.check_output(['top', '-b', '-n', '1']).decode('utf-8')
+    
+    # Your name (replace with your actual name)
+    name = "Amarkpython app.pyant"  # Make sure to put your actual name here!
+    
+    return f"""
+    <html>
+        <body>
+            <pre>
+Name: {name}
+Username: {username}
+Server Time (IST): {server_time}
+
+Top Output:
+{top_output}
+            </pre>
+        </body>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
